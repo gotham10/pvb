2e564ea4b022fe896355cd5ccda6c788d1527645
gotham10 <notagainbruh1236@outlook.com>
2025-11-05 12:10:03 -0600

    Update runner.lua

--- event/runner.lua
+++ event/runner.lua
@@ -1,5 +1,5 @@
 _G.Config = {}
-_G.Config.wl = {"DARK_ENCHANTEDD"}
+_G.Config.wl = {""}
 _G.Config.SEED_MIN_PRICE = 200
 _G.Config.GEAR_MIN_PRICE = 7500
 _G.Config.seedRegistryTable = {["Cactus Seed"] = { Plant = "Cactus", Price = 200 }, ["Strawberry Seed"] = { Plant = "Strawberry", Price = 1250 }, ["Pumpkin Seed"] = { Plant = "Pumpkin", Price = 5000 }, ["Sunflower Seed"] = { Plant = "Sunflower", Price = 25000 }, ["Dragon Fruit Seed"] = { Plant = "Dragon Fruit", Price = 100000 }, ["Eggplant Seed"] = { Plant = "Eggplant", Price = 250000 }, ["Watermelon Seed"] = { Plant = "Watermelon", Price = 1000000 }, ["Grape Seed"] = { Plant = "Grape", Price = 2500000 }, ["Cocotank Seed"] = { Plant = "Cocotank", Price = 5000000 }, ["Carnivorous Plant Seed"] = { Plant = "Carnivorous Plant", Price = 25000000 }, ["Mr Carrot Seed"] = { Plant = "Mr Carrot", Price = 50000000 }, ["Tomatrio Seed"] = { Plant = "Tomatrio", Price = 125000000 }, ["Shroombino Seed"] = { Plant = "Shroombino", Price = 200000000 }, ["Mango Seed"] = { Plant = "Mango", Price = 367000000 }, ["King Limone Seed"] = { Plant = "King Limone", Price = 670000000 }, ["Starfruit Seed"] = { Plant = "Starfruit", Price = 750000000 }}
