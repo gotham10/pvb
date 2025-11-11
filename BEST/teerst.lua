3dd11e78bbeb3c60d4c6f3ad12d8ef9581dd45fc
gotham10 <notagainbruh1236@outlook.com>
2025-11-10 21:54:49 -0600

    Create teerst.lua

---
+++ BEST/teerst.lua
@@ -0,0 +1,4 @@
+auto sell all plants:
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ItemSell"):FireServer(nil,true,true)
+auto sell all brainrots:
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ItemSell"):FireServer(nil,nil,true)
